fb6499ebc4941fdccf15bb6c9ad92dda0e04c4c2
Florent Barriol <fbarriol.externe@m6.fr>
2019-06-09 18:33:38 +0200

    create not found page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-);
+import GIF from '../../content/assets/turkey.gif';
+
+const Wrapper = styled.div`
+  text-align: center;
+`;
+
+const NotFoundPage = () => {
+  return (
+    <Layout>
+      <SEO title="Page non trouvée: Aïe" />
+      <Wrapper>
+        <h1>
+          <span aria-label="Aïe" role="img">
+            😱
+          </span>{' '}
+          Aïe...
+        </h1>
+        <p>
+          Cette page n'existe pas, c'est la tristesse...
+          <br />
+          Mais vous aurez au moins vu ça :
+          <br />
+          <br />
+          <img src={GIF} alt="The GIF" />
+        </p>
+      </Wrapper>
+    </Layout>
+  );
+};
 
 export default NotFoundPage;
